251d1b6672bae0ced0cd87cddc4acd96ea16777d
Nikita Polozov <npolozov0@gmail.com>
2023-01-09 10:03:54 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  contact: contactsReducer,
-  filter: filterReducer,
+  reducer: {
+    contact: contactsReducer,
+    filter: filterReducer,
+  },
 });
